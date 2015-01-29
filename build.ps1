f76627f158479f6bb7c815e3ff033297cffebb9f
sriv <srikanth.ddit@gmail.com>
2015-01-29 17:01:09 +0530

    use nuget restore instead of packagerestore in msbuild

--- build.ps1
+++ build.ps1
@@ -2,6 +2,12 @@ $outputPath= [IO.Path]::Combine($pwd,"artifacts")
 Remove-Item $outputPath -recurse
 New-Item -Itemtype directory $outputPath -Force
 $msbuild="$($env:systemroot)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$nuget=".\.nuget\nuget.exe"
 $sln = "Gauge.VisualStudio.sln"
+
+Write-Host -ForegroundColor Yellow "Restoring Nuget Packages..."
+&$nuget restore
+Write-Host -ForegroundColor Yellow "Done."
+
 
 &$msbuild $sln /m /nologo "/p:configuration=release;OutDir=$($outputPath)" /t:rebuild
