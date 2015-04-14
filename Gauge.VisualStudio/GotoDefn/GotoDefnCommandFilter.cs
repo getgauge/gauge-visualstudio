3964ac7bd781d3397ad2ba7507ccb5fbe938c47c
sriv <srikanth.ddit@gmail.com>
2015-04-14 14:53:48 +0530

    added apache 2.0 license

--- Gauge.VisualStudio/GotoDefn/GotoDefnCommandFilter.cs
+++ Gauge.VisualStudio/GotoDefn/GotoDefnCommandFilter.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿// Copyright [2014, 2015] [ThoughtWorks Inc.](www.thoughtworks.com)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using EnvDTE;
 using Gauge.VisualStudio.Classification;
 using Gauge.VisualStudio.Models;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Project = Gauge.VisualStudio.Models.Project;
 
