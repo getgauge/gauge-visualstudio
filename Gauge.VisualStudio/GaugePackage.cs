a189eb4adacfc4d41a933cd5a6e5384120eb871e
sriv <srikanth.ddit@gmail.com>
2015-06-26 19:40:03 +0530

    bumped up to version 0.0.6, also added target framework 4.6 support, fixes #38

--- Gauge.VisualStudio/GaugePackage.cs
+++ Gauge.VisualStudio/GaugePackage.cs
@@ -25,21 +25,7 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Gauge.VisualStudio
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the information needed to show this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid(GuidList.GuidGaugeVsPackagePkgString)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
@@ -49,28 +35,6 @@ namespace Gauge.VisualStudio
         private SolutionEventsListener _solutionEventsListener;
         private Events2 _dteEvents;
         private CodeModelEvents _documentEvents;
-
-        /// <summary>
-        /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
-        /// initialization is the Initialize method.
-        /// </summary>
-        public GaugePackage()
-        {
-            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
-        }
-
-
-        /////////////////////////////////////////////////////////////////////////////
-        // Overridden Package Implementation
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
 
         protected override void Initialize()
         {
@@ -83,7 +47,6 @@ namespace Gauge.VisualStudio
             var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (null != mcs)
             {
-                // Create the command for the menu item.
                 var menuCommandId = new CommandID(GuidList.GuidGaugeVsPackageCmdSet, (int)PkgCmdIdList.formatCommand);
                 var menuItem = new OleMenuCommand(MenuItemCallback, menuCommandId);
                 menuItem.BeforeQueryStatus += delegate(object sender, EventArgs args)
@@ -95,10 +58,10 @@ namespace Gauge.VisualStudio
                         menuCommand.Visible = false;
                         menuCommand.Enabled = false;
 
-                        string itemFullPath = null;
+                        string itemFullPath;
 
-                        IVsHierarchy hierarchy = null;
-                        var itemid = VSConstants.VSITEMID_NIL;
+                        IVsHierarchy hierarchy;
+                        uint itemid;
 
                         if (!IsSingleProjectItemSelection(out hierarchy, out itemid)) return;
 
@@ -121,19 +84,12 @@ namespace Gauge.VisualStudio
             base.Initialize();
         }
 
-        #endregion
-
-        /// <summary>
-        ///     This function is the callback used to execute a command when the a menu item is clicked.
-        ///     See the Initialize method to see how the menu item is associated to this function using
-        ///     the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            string itemFullPath = null;
+            string itemFullPath;
 
-            IVsHierarchy hierarchy = null;
-            var itemid = VSConstants.VSITEMID_NIL;
+            IVsHierarchy hierarchy;
+            uint itemid;
 
             if (!IsSingleProjectItemSelection(out hierarchy, out itemid)) return;
 
@@ -182,14 +138,10 @@ namespace Gauge.VisualStudio
 
                 if (ErrorHandler.Failed(hr) || hierarchyPtr == IntPtr.Zero || itemid == VSConstants.VSITEMID_NIL)
                 {
-                    // there is no selection
                     return false;
                 }
 
-                // multiple items are selected
                 if (multiItemSelect != null) return false;
-
-                // there is a hierarchy root node selected, thus it is not a single item inside a project
 
                 if (itemid == VSConstants.VSITEMID_ROOT) return false;
 
@@ -198,13 +150,7 @@ namespace Gauge.VisualStudio
 
                 Guid guidProjectId;
 
-                if (ErrorHandler.Failed(solution.GetGuidOfProject(hierarchy, out guidProjectId)))
-                {
-                    return false; // hierarchy is not a project inside the Solution if it does not have a ProjectID Guid
-                }
-
-                // if we got this far then there is a single project item selected
-                return true;
+                return !ErrorHandler.Failed(solution.GetGuidOfProject(hierarchy, out guidProjectId));
             }
             finally
             {
