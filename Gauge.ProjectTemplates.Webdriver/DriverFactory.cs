a778992b2e527a260ab6bce7aff658d0b0f6a0d7
sriv <srikanth.ddit@gmail.com>
2016-04-21 16:43:01 +0530

    added template for Gauge Webdriver

---
+++ Gauge.ProjectTemplates.Webdriver/DriverFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace $safeprojectname$
+{
+    public class DriverFactory
+    {
+        public static IWebDriver GetDriver()
+        {
+            var browser = Environment.GetEnvironmentVariable("BROWSER");
+            switch (browser)
+            {
+                case "chrome":
+                    return new ChromeDriver();
+                case "ie":
+                    return new InternetExplorerDriver();
+                default:
+                    return new FirefoxDriver();
+            }
+        }
+    }
+}
---
+++ Gauge.ProjectTemplates.Webdriver/specs/example.spec
@@ -0,0 +1,13 @@
+﻿Getting Started with Gauge
+==========================
+
+This is an executable specification file. This file follows markdown syntax. Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+To execute this specification, use
+	mvn test
+
+* Navigate to Gauge homepage "http://getgauge.io"
+
+Get Started
+-----------
+
+* Go to Gauge Get Started Page
