c41672a9239f57d85ab6e43cdd2ff867d3a5fe6f
sriv <srikanth.ddit@gmail.com>
2016-05-18 17:31:29 +0530

    build.bat runs tests too

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 @echo off
 powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File ".\build.ps1"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File ".\test.ps1"
+if %errorlevel% neq 0 exit /b %errorlevel%
