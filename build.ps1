2703a5010700e36a9c7b7fb9b635d4f73ea2308a
sriv <srikanth.ddit@gmail.com>
2016-05-30 14:48:29 +0530

    include nightly in display name

--- build.ps1
+++ build.ps1
@@ -1,3 +1,17 @@
+# Copyright [2014, 2015] [ThoughtWorks Inc.](www.thoughtworks.com)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 $outputPath= [IO.Path]::Combine($pwd,"artifacts")
 If (Test-Path $outputPath)
 {
@@ -17,6 +31,13 @@ $verbosity = "minimal"
 if($env:MSBUILD_VERBOSITY)
 {
   $verbosity = $env:MSBUILD_VERBOSITY
+}
+
+if($env:NIGHTLY)
+{
+  $nightly="nightly-$(Get-Date -F "yyyy-MM-dd")"
+  Write-Host -ForegroundColor yellow "Making Nightly : $nightly"
+  & "$(Split-Path $MyInvocation.MyCommand.Path)\version_nightly.ps1" -nightly $nightly
 }
 
 &$msbuild $sln /m /nologo "/p:configuration=release;OutDir=$($outputPath);VisualStudioVersion=12.0" /t:rebuild /verbosity:$($verbosity)
