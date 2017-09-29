3a70e4135e54ef29bbfcf9a426d104813af040e9
sriv <srikanth.ddit@gmail.com>
2017-09-29 15:47:46 +0530

    remove grpc/execution API code
    remove templates from vsix build

--- Gauge.VisualStudio.TestAdapter/TestExecutor.cs
+++ Gauge.VisualStudio.TestAdapter/TestExecutor.cs
@@ -14,8 +14,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
@@ -28,57 +26,17 @@ namespace Gauge.VisualStudio.TestAdapter
         public const string ExecutorUriString = "executor://gaugespecexecutor/v1";
         public static readonly Uri ExecutorUri = new Uri(ExecutorUriString);
         private bool _cancelled;
-        private readonly GaugeRunnerV2 _gaugeRunnerV2 = new GaugeRunnerV2();
         private readonly GaugeRunner _gaugeRunner = new GaugeRunner();
 
         public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
         {
-            var gaugeTestRunSettingsService = runContext.RunSettings.GetSettings(GaugeTestRunSettings.SettingsName) as IGaugeTestRunSettingsService;
-            if (gaugeTestRunSettingsService.Settings.UseExecutionAPI)
-            {
-                frameworkHandle.SendMessage(TestMessageLevel.Informational, "UseExecutionAPI=true, executing specs via Gauge API.");
-                RunTestsV2(tests, runContext, frameworkHandle);
-                return;
-            }
-            frameworkHandle.SendMessage(TestMessageLevel.Informational, "UseExecutionAPI=false, invoking gauge.exe for test run.");
-            RunTestsV1(tests, runContext, frameworkHandle);
-        }
-
-        private void RunTestsV1(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
-        {
+            frameworkHandle.SendMessage(TestMessageLevel.Informational, "invoking gauge.exe for test run.");
             _cancelled = false;
             foreach (var testCase in tests)
             {
                 if (_cancelled) break;
                 _gaugeRunner.Run(testCase, runContext.IsBeingDebugged, frameworkHandle);
             }
-        }
-
-        private void RunTestsV2(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
-        {
-            var testSuites = new Dictionary<int, List<TestCase>>();
-            _cancelled = false;
-            foreach (var testCase in tests)
-            {
-                if (_cancelled) break;
-                var port = testCase.GetPropertyValue(TestDiscoverer.GaugeApiV2Port, -1);
-                if (!testSuites.ContainsKey(port))
-                {
-                    testSuites.Add(port, new List<TestCase>());
-                }
-                testSuites[port].Add(testCase);
-            }
-            Func<KeyValuePair<int, List<TestCase>>, Task> selector;
-            if (runContext.IsBeingDebugged)
-            {
-                selector = suite => _gaugeRunnerV2.Debug(suite.Value, suite.Key, frameworkHandle);
-            }
-            else
-            {
-                selector = suite => _gaugeRunnerV2.Run(suite.Value, suite.Key, frameworkHandle);
-            }
-            var tasks = testSuites.Select(selector);
-            Task.WaitAll(tasks.ToArray());
         }
 
         public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
